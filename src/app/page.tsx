366fa9fdd2daf1ad18c8e5335fd2d8f9c5d3820b
Indra Wardana <indrawardana2003@gmail.com>
2025-12-12 15:00:01 +0000

    untuk game info tetap di tampilkan meskipun berada di score per game

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,17 +9,15 @@ export default function Home() {
     <div className="min-h-screen w-full bg-custom-background bg-cover bg-center relative flex flex-col">
       <div className="absolute inset-0 bg-black/60 z-0" />
       <div className="container flex-grow max-w-screen-xl mx-auto py-4 sm:py-8 relative z-10">
-        <main>
+        <main className="space-y-4">
+          <GameInfo />
           <Tabs defaultValue="total" className="w-full">
             <TabsList className="grid w-full grid-cols-2">
               <TabsTrigger value="total">Skor Total</TabsTrigger>
               <TabsTrigger value="history">Skor per Game</TabsTrigger>
             </TabsList>
             <TabsContent value="total" className="mt-4">
-              <div className="space-y-8">
-                <GameInfo />
-                <Leaderboard />
-              </div>
+               <Leaderboard />
             </TabsContent>
             <TabsContent value="history" className="mt-4">
               <GlobalScoreHistory />

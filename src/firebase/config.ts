59acb37dee02fd5c6b314b6be2c1ae61f4dd5132
Indra Wardana <indrawardana2003@gmail.com>
2025-12-16 13:18:13 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2395898610-e02bc",
+  "appId": "1:788049512962:web:b8e47592121bde71bd751b",
+  "apiKey": "AIzaSyAvL7eqAdG3qnYPo7WogWy7BkCoNBZX-js",
+  "authDomain": "studio-2395898610-e02bc.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "788049512962"
+};
